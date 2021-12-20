add747385ba0a7a4b0cfba115db090bc6d390cb6
Genilton Souza <genilton.souza26@gmail.com>
2021-12-20 11:34:29 -0400

    Lesson 30
    Adding & Configuring the Submit Button

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -4,19 +4,20 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
-
 @NgModule({
   imports: [
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
+    MatButtonModule,
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
+    MatButtonModule,
   ],
 })
 export class MaterialModule {}
