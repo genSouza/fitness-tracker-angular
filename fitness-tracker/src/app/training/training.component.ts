2930d2619a0ceb01ab52bdbd8f4b0788067e182c
Genilton Souza <genilton.souza26@gmail.com>
2021-12-29 14:58:29 -0400

    Lesson 66
     Controlling the Active Exercise

--- fitness-tracker/src/app/training/training.component.ts
+++ fitness-tracker/src/app/training/training.component.ts
@@ -1,5 +1,6 @@
+import { TrainingService } from './training.service';
 import { Component, OnInit } from '@angular/core';
-
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainingComponent implements OnInit {
   ongoingTraining = false;
-  constructor() {}
+  exerciseSubscription!: Subscription;
 
-  ngOnInit(): void {}
+  constructor(private trainingService: TrainingService) {}
+
+  ngOnInit(): void {
+    this.exerciseSubscription = this.trainingService.exerciseChanged.subscribe(
+      (exercise) => {
+        if (exercise) {
+          this.ongoingTraining = true;
+        } else {
+          this.ongoingTraining = false;
+        }
+      }
+    );
+  }
 }
--- fitness-tracker/src/app/training/training.service.ts
+++ fitness-tracker/src/app/training/training.service.ts
@@ -1,6 +1,8 @@
 import { Exercise } from './exercise.model';
-
+import { Subject } from 'rxjs';
 export class TrainingService {
+  exerciseChanged = new Subject<Exercise>();
+
   private availableExercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
@@ -8,15 +10,17 @@ export class TrainingService {
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 },
   ];
 
-  private runningExercise!: Exercise | undefined;
+  private runningExercise!: Exercise;
 
   getAvailableExercises() {
     return this.availableExercises.slice();
   }
 
   startExercise(selectId: string) {
-    this.runningExercise = this.availableExercises.find(
-      (item) => item.id === selectId
-    );
+    const find = this.availableExercises.find((item) => item.id === selectId);
+    if (find) {
+      this.runningExercise = find;
+    }
+    this.exerciseChanged.next({ ...this.runningExercise });
   }
 }
