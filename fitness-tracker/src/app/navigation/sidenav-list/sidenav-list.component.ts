0ca5b181a4ee099a75a04bde4dc9dd1c71df377f
Genilton Souza <genilton.souza26@gmail.com>
2021-12-29 12:01:18 -0400

    Lesson 61
    Routing & Authentication

--- fitness-tracker/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ fitness-tracker/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,21 +1,41 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AuthService } from './../../auth/auth.service';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
-  styleUrls: ['./sidenav-list.component.scss']
+  styleUrls: ['./sidenav-list.component.scss'],
 })
-export class SidenavListComponent implements OnInit {
-
+export class SidenavListComponent implements OnInit, OnDestroy {
   @Output()
   closeSideNav = new EventEmitter<void>();
-
-  constructor() { }
+  isAuth = false;
+  authSubscription!: Subscription;
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe((result) => {
+      this.isAuth = result;
+    });
   }
 
   onClose() {
     this.closeSideNav.emit();
+  }
+
+  onLogout() {
+    this.onClose();
+    this.authService.logout();
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
   }
 }
